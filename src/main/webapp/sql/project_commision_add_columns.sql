5e5dc1b17827aa501486dfa3b3000819991a9458
louis.liu <lssmantwin@163.com>
2017-04-23 23:40:56 +0800

    Project commision

---
+++ src/main/webapp/sql/project_commision_add_columns.sql
@@ -0,0 +1,3 @@
+alter table project_commision add first_commision_rate decimal(4,4);
+alter table project_commision add balance_commision_rate decimal(4,4);
+alter table project_commision add purchase_agent_contract_fee decimal(4,4);
