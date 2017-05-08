1b6df0c570af5419f20d5cc10908020a1fb01cae
willzha <willzha@163.com>
2017-05-08 23:07:46 +0800

    excel header to be chinese

--- src/main/java/com/dms/constant/DmsConstant.java
+++ src/main/java/com/dms/constant/DmsConstant.java
@@ -1,14 +1,12 @@
 package com.dms.constant;
 
-import com.google.common.collect.Lists;
-
 import java.util.List;
+
+import com.google.common.collect.Lists;
 
 public final class DmsConstant {
 
-	public static List<String> projectCommissionHeader = Lists.newArrayList("designer", "designerAssistant", "acNumber", "customerName", "contractTotal",
-			"purchaseAgentFee", "projectChangeTotal", "customerPay", "payContractRatio", "payProjectRatio", "contractState", "commissionState",
-			"firstCommission", "balanceCommission", "designCommissionRate", "contractDate", "firstCommissionDate", "actualStartTime", "actualEndTime",
-			"balanceTime", "balanceCommissionDate", "designerAssistantCommissionRate");
+	public static List<String> projectCommissionHeader = Lists.newArrayList("设计师", "设计助理", "受理编号", "客户名字", "合同价", "采购代理费用", "变更总价", "已付", "合同收款比", "总收款比", "状态",
+			"提成状态", "首提", "二次提成", "提成系数", "签约日期", "首提日期", "实际开工", "实际竣工", "结算日期", "二次提成日期", "提成比");
 
 }
